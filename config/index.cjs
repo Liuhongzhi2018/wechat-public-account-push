8edce70deb20749ad0c67c7246d6d692281bb7f3
Liuhongzhi2018 <38147672+Liuhongzhi2018@users.noreply.github.com>
2023-07-15 23:55:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-03-04' },
+        { keyword: 'love_day', date: '2022-03-31' },
         // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-03-04' },
       ],
     },
   ],
